231c5282235af25241acd25dc8c93cce4ceb4104
DurgeshKumar143 <dk0078774@gmail.com>
2024-05-01 20:49:54 +0530

    update

--- src/components/Auth/Ragistar.jsx
+++ src/components/Auth/Ragistar.jsx
@@ -79,7 +79,7 @@ const Ragistar = () => {
     <section className="authPage">
       <div className="container">
         <div className="header">
-          <img src="/JobZeelogo.png" alt="logo" />
+          {/*<img src="/JobZeelogo.png" alt="logo" />*/}
           <h3>Create a new account</h3>
         </div>
         <form>
